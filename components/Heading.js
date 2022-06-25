bf1579016d0ef23f686fa3c149afffb65dc02a94
Kunal Bagaria <61944452+KunalBagaria@users.noreply.github.com>
2022-06-25 22:29:28 +0530

    update heading

--- components/Heading.js
+++ components/Heading.js
@@ -2,7 +2,7 @@ import styles from '../styles/Home.module.scss'
 
 const Heading = () => (
     <h1 className={styles.heading}>
-        Hi, I’m Kunal Bagaria. I’m a developer
+        Hi, I’m Kunal Bagaria. I’m a software engineer
     </h1>
 )
 
